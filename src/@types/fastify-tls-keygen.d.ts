fc45f0ca9386f9a2e42ef86b12776a66adc26993
Łukasz Wójcik <lwojcik@lukem.net>
2019-06-09 19:27:47 +0200

    further polishing

--- src/@types/fastify-tls-keygen.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'fastify-tls-keygen';
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
 require('dotenv').config();
 import fastify, { ServerOptions, Plugin } from 'fastify';
 import { Server, IncomingMessage, ServerResponse } from 'http';
-// const rateLimit = require('fastify-rate-limit');
 import blipp from 'fastify-blipp';
-// const healthcheck = require('fastify-healthcheck');
-// const compression = require('fastify-compress');
-import helmet from 'fastify-helmet';
-// const fastifyCaching = require('fastify-caching');
-// const fastifyRedis = require('fastify-redis');
-// import Redis from 'ioredis';
-// import AbstractCache from 'abstract-cache';
+import fastifyCaching from 'fastify-caching';
+import fastifyRedis from 'fastify-redis';
+import Redis from 'ioredis';
+import AbstractCache from 'abstract-cache';
 
 import appConfig from './config/app';
-// const redisConfig = require('./config/redis');
+import redisConfig from './config/redis';
 
 import * as routes from './routes/index';
-
-// const db = require('./plugins/db');
 
 /* Fastify plugin types */
 
@@ -32,34 +26,43 @@ type FastifyPlugins = (FastifyPlugin | FastifyPluginObject)[];
 /* Server instance */
 
 const server = fastify({
-  logger: process.env.NODE_ENV === 'development',
-  https: process.env.API_HOST_PROTOCOL === 'https',
+  logger: process.env.NODE_ENV === 'development'
 } as ServerOptions);
 
-/* Server plugins */
+/* Caching */
 
-// /* Caching */
-
-// 
-// const abcache = new AbstractCache({
-//   useAwait: true,
-//   driver: {
-//     name: 'abstract-cache-redis',
-//     options: {
-//       client: redisClient,
-//       cacheSegment: 'sc2pte-cache',
-//     },
-//   },
-// });
-
-// const redisClient = new Redis(redisConfig.connectionString);
+const redisClient = new Redis(redisConfig.connectionString);
 
 const plugins = [
   /* Display the routes table to console at startup */
   blipp,
 
-  /* Important security headers */
-  helmet,
+  /* Caching */
+
+  {
+    plugin: fastifyRedis,
+    options: {
+      client: redisClient,
+    },
+  },
+
+  {
+    plugin: fastifyCaching,
+    options: {
+      cache: new AbstractCache({
+        useAwait: true,
+        driver: {
+          name: 'abstract-cache-redis',
+          options: {
+            client: redisClient,
+            cacheSegment: 'bas-cache',
+          },
+        },
+      }),
+      expiresIn: 5 * 60, // seconds
+      cacheSegment: process.env.API_REDIS_CACHE_SEGMENT,
+    },
+  },
 
   /* Routes */
   routes.checkAccessToken,
@@ -67,25 +70,6 @@ const plugins = [
   routes.getAccessToken,
   routes.refreshAccessToken,
 ] as FastifyPlugins;
-
-// server.register(fastifyRedis, { client: redisClient });
-// server.register(fastifyCaching, {
-//   cache: abcache,
-//   expiresIn: 5 * 60, // seconds
-//   cacheSegment: env.API_REDIS_CACHE_SEGMENT,
-// });
-
-// /* Plugins */
-
-// server.register(compression);
-// server.register(helmet);
-// server.register(rateLimit, {
-//   max: 100,
-//   timeWindow: '1 minute',
-//   redis: redisClient,
-//   whitelist: ['127.0.0.1'],
-//   skipOnError: true,
-// });
 
 /* Registering server plugins */
 
