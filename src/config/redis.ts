aee6469948dcb05ff17d9f9e4fe8af9e9aaa9384
Łukasz Wójcik <lwojcik@lukem.net>
2019-06-18 00:04:56 +0200

    minor config tweaks

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -8,7 +8,7 @@
 const redisConfig = {
   enable: process.env.API_REDIS_ENABLE === 'true' || false,
   connectionString: process.env.API_REDIS_CONNECTION_STRING || 'redis://127.0.0.1:6379',
-  db: process.env.API_REDIS_DB || '4',
+  db: process.env.API_REDIS_DB || '0',
   replyCachePeriod: process.env.API_REDIS_CACHE_PERIOD || 1000 * 60 * 5,
   cacheSegment: process.env.API_REDIS_CACHE_SEGMENT || 'bas',
 };
