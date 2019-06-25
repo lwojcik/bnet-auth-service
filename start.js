dcdc1270afd3ff63aa7a76f1fe650761cc70b596
Łukasz Wójcik <lwojcik@lukem.net>
2019-06-26 00:10:51 +0200

    rename env vars

--- start.js
+++ start.js
@@ -8,19 +8,19 @@ const server = require('./dist/index');
 const opts = {
   app: {
     nodeEnv: process.env.NODE_ENV || 'development',
-    port: process.env.API_NODE_PORT || '8881',
+    port: process.env.BAS_NODE_PORT || '8881',
   },
   redis: {
-    enable: process.env.API_REDIS_ENABLE === 'true' || false,
-    connectionString: process.env.API_REDIS_CONNECTION_STRING || 'redis://127.0.0.1:6379',
-    db: process.env.API_REDIS_DB || '0',
-    replyCachePeriod: process.env.API_REDIS_CACHE_PERIOD || 1000 * 60 * 5,
-    cacheSegment: process.env.API_REDIS_CACHE_SEGMENT || 'bas',
+    enable: process.env.BAS_REDIS_ENABLE === 'true' || false,
+    connectionString: process.env.BAS_REDIS_CONNECTION_STRING || 'redis://127.0.0.1:6379',
+    db: process.env.BAS_REDIS_DB || '0',
+    replyCachePeriod: process.env.BAS_REDIS_CACHE_PERIOD || 1000 * 60 * 5,
+    cacheSegment: process.env.BAS_REDIS_CACHE_SEGMENT || 'bas',
   },
   bnet: {
-    region: process.env.API_BATTLENET_REGION,
-    apiKey: process.env.API_BATTLENET_KEY,
-    apiSecret: process.env.API_BATTLENET_SECRET,
+    region: process.env.BAS_BATTLENET_REGION,
+    apiKey: process.env.BAS_BATTLENET_KEY,
+    apiSecret: process.env.BAS_BATTLENET_SECRET,
   }
 }
 
