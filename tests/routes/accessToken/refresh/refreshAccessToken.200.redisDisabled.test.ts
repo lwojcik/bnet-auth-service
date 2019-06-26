d830f3cc9003f0c00d45aae16e6c6bab3d0af5f4
Łukasz Wójcik <lwojcik@lukem.net>
2019-06-26 20:57:40 +0200

    splitting test files

--- tests/routes/accessToken/refresh/refreshAccessToken.200.redisDisabled.test.ts
+++ tests/routes/accessToken/refresh/refreshAccessToken.200.redisDisabled.test.ts
@@ -1,6 +1,6 @@
 const fastify = require('fastify');
 const fp = require('fastify-plugin');
-const server = require('../../src/index');
+const server = require('../../../../src/index');
 
 const config = {
   app: {
@@ -17,16 +17,6 @@ const config = {
 const redisDisabled = {
   redis: {
     enable: false,
-  }
-}
-
-const redisEnabled = {
-  redis: {
-    enable: true,
-    connectionString: 'redis://127.0.0.1:6379',
-    db: '0',
-    replyCachePeriod: 100,
-    cacheSegment: 'bas',
   }
 }
 
@@ -55,34 +45,5 @@ describe('/accessToken/refresh (Redis disabled)', () => {
   it('returns correct response', async () => {
     const res = await fastifyServer.inject({ method: 'GET', url: '/accessToken/refresh', });
     expect(JSON.parse(res.payload)).toEqual({ status:200, message: "Access token not refreshed (Redis disabled)"});
-  });
-});
-
-describe('/accessToken/refresh (Redis enabled)', () => {
-  const fastifyServer = fastify();
-  jest.mock('ioredis');
-
-  beforeAll(() => {
-    fastifyServer.register(server, { ...config, ...redisEnabled });
-  });
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-    jest.resetModules();
-  });
-
-  afterEach(() => {
-    fastifyServer.close();
-  });
- 
-  it('returns 200', async () => {
-    const res = await fastifyServer.inject({ method: 'GET', url: '/accessToken/refresh', });
-    expect(res.statusCode).toBe(200);
-  });
-
-
-  it('returns correct response', async () => {
-    const res = await fastifyServer.inject({ method: 'GET', url: '/accessToken/refresh', });
-    expect(JSON.parse(res.payload)).toEqual({ status:200, message: "Access token refreshed successfully" });
   });
 });
---
+++ tests/routes/accessToken/refresh/refreshAccessToken.200.redisEnabled.test.ts
@@ -0,0 +1,84 @@
+const fastify = require('fastify');
+const fp = require('fastify-plugin');
+const server = require('../../../../src/index');
+
+const config = {
+  app: {
+    nodeEnv: 'test',
+    port: '8123',
+  },
+  bnet: {
+    region: 'us',
+    apiKey: 'key',
+    apiSecret: 'secret',
+  }
+}
+
+const redisEnabled = {
+  redis: {
+    enable: true,
+    connectionString: 'redis://127.0.0.1:6379',
+    db: '0',
+    replyCachePeriod: 100,
+    cacheSegment: 'bas',
+  }
+}
+
+describe('/accessToken/refresh (Redis enabled)', () => {
+  jest.mock('ioredis');
+
+  const fastifyServer = fastify();
+
+  beforeAll(() => {
+    fastifyServer.register(server, { ...config, ...redisEnabled });
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+  });
+
+  afterEach(() => {
+    fastifyServer.close();
+  });
+ 
+  it('returns 200', async () => {
+    const res = await fastifyServer.inject({ method: 'GET', url: '/accessToken/refresh', });
+    expect(res.statusCode).toBe(200);
+  });
+
+
+  it('returns correct response', async () => {
+    const res = await fastifyServer.inject({ method: 'GET', url: '/accessToken/refresh', });
+    expect(JSON.parse(res.payload)).toEqual({ status:200, message: "Access token refreshed successfully"});
+  });
+});
+
+describe('/accessToken/refresh (Redis enabled)', () => {
+  const fastifyServer = fastify();
+  jest.mock('ioredis');
+
+  beforeAll(() => {
+    fastifyServer.register(server, { ...config, ...redisEnabled });
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+  });
+
+  afterEach(() => {
+    fastifyServer.close();
+  });
+ 
+  it('returns 200', async () => {
+    const res = await fastifyServer.inject({ method: 'GET', url: '/accessToken/refresh', });
+    expect(res.statusCode).toBe(200);
+  });
+
+
+  it('returns correct response', async () => {
+    const res = await fastifyServer.inject({ method: 'GET', url: '/accessToken/refresh', });
+    expect(JSON.parse(res.payload)).toEqual({ status:200, message: "Access token refreshed successfully" });
+  });
+});
