aaf28aed4a9a7032a5d87856caea5e94f523e15d
Łukasz Wójcik <lwojcik@lukem.net>
2019-06-28 23:59:29 +0200

    final fixes

--- start.js
+++ start.js
@@ -5,27 +5,6 @@ const fastifyRedis = require('fastify-redis');
 const fastifyEnv = require('fastify-env');
 const server = require('./dist/index');
 
-const opts = {
-  app: {
-    nodeEnv: process.env.NODE_ENV || 'development',
-    port: process.env.BAS_NODE_PORT || '8881',
-  },
-  redis: {
-    enable: process.env.BAS_REDIS_ENABLE === 'true' || false,
-    host: process.env.BAS_REDIS_HOST || '127.0.0.1',
-    port: process.env.BAS_REDIS_PORT || '6379',
-    password: process.env.BAS_REDIS_PASSWORD || '',
-    db: process.env.BAS_REDIS_DB || '0',
-    replyCachePeriod: process.env.BAS_REDIS_CACHE_PERIOD || 1000 * 60 * 5,
-    cacheSegment: process.env.BAS_REDIS_CACHE_SEGMENT || 'bas',
-  },
-  bnet: {
-    region: process.env.BAS_BATTLENET_REGION,
-    apiKey: process.env.BAS_BATTLENET_KEY,
-    apiSecret: process.env.BAS_BATTLENET_SECRET,
-  }
-}
-
 const envSchema = {
   type: 'object',
   required: [
@@ -88,6 +67,27 @@ const envSchema = {
     BAS_BATTLENET_SECRET: {
       type: 'string',
     }
+  }
+}
+
+const opts = {
+  app: {
+    nodeEnv: process.env.NODE_ENV || 'development',
+    port: process.env.BAS_NODE_PORT || '8881',
+  },
+  redis: {
+    enable: process.env.BAS_REDIS_ENABLE === 'true' || false,
+    host: process.env.BAS_REDIS_HOST || '127.0.0.1',
+    port: process.env.BAS_REDIS_PORT || '6379',
+    password: process.env.BAS_REDIS_PASSWORD || '',
+    db: process.env.BAS_REDIS_DB || '0',
+    replyCachePeriod: process.env.BAS_REDIS_CACHE_PERIOD || 1000 * 60 * 5,
+    cacheSegment: process.env.BAS_REDIS_CACHE_SEGMENT || 'bas',
+  },
+  bnet: {
+    region: process.env.BAS_BATTLENET_REGION,
+    apiKey: process.env.BAS_BATTLENET_KEY,
+    apiSecret: process.env.BAS_BATTLENET_SECRET,
   }
 }
 
