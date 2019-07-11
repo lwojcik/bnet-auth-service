94d77d74e8d241c13d12b2e45455b8420e37b0ca
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2019-07-11 18:24:33 +0200

    redis cache tests (#18)

--- tests/routes/accessToken/get/getAccessToken.200.redisEnabled.test.ts
+++ tests/routes/accessToken/get/getAccessToken.200.redisEnabled.test.ts
@@ -4,7 +4,7 @@ import server from '../../../../src/index';
 import getConfig from '../../../helper';
 
 describe('/accessToken/get 200 (Redis enabled)', () => {
-  const fastifyServer = fastify();
+  const fastifyServer = fastify() as any;
 
   beforeAll(async () => {
     fastifyServer.register(fastifyRedis, {
@@ -45,5 +45,11 @@ describe('/accessToken/get 200 (Redis enabled)', () => {
   it('returns correct response refresh parameter is sent', async () => {
     const res = await fastifyServer.inject({ method: 'GET', url: '/accessToken/get?refresh=true', });
     expect(JSON.parse(res.payload)).toEqual({"status":200,"data":{ accessToken: "sample access token" }});
+  });
+
+  it('response is cached correctly', async () => {
+    await fastifyServer.inject({ method: 'GET', url: '/accessToken/get?refresh=true', });
+    const cachedResponse = await fastifyServer.redis.get(getConfig(true).redis.cacheSegment);
+    expect(cachedResponse).toEqual("sample access token");
   });
 });
