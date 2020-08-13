4cd21e17847fd9b0d10257c1b41576d0f2cd84b3
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2020-08-13 19:29:51 +0200

    Fixes for Fastify 3 (#187)
    * update dependencies
    
    * fix type errors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from 'fastify';
 import fp from 'fastify-plugin';
-import * as routes from './routes';
 import accessToken from './plugins/accessToken';
+import * as routes from './routes';
 
 interface ServerOptions {
   app: {
@@ -25,11 +25,10 @@ interface ServerOptions {
 
 const server = fp(
   (fastify: FastifyInstance, opts: ServerOptions, next: Function) => {
-    fastify.register(routes.status);
-    fastify.register(routes.getAccessToken, opts.bnet);
-    fastify.register(routes.refreshAccessToken, opts.bnet);
     fastify.register(accessToken, { bnet: opts.bnet, redis: opts.redis });
-
+    fastify.register(routes.status);
+    fastify.register(routes.getAccessToken);
+    fastify.register(routes.refreshAccessToken);
     next();
   },
 );
