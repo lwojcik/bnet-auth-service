bdcd825fb4b944add5ebe4214802d28a6f709245
Łukasz Wójcik <lwojcik@lukem.net>
2021-11-20 02:45:42 +0100

    Suppress lint errors

--- scripts/start.js
+++ scripts/start.js
@@ -101,6 +101,7 @@ const fastifyInstance = fastify({
   logger: opts.app.nodeEnv === 'development',
 });
 
+// eslint-disable-next-line jest/require-hook
 fastifyInstance.register(fastifyEnv, {
   schema: envSchema,
   dotenv: {
@@ -119,7 +120,9 @@ if (process.env.BAS_REDIS_ENABLE === 'true') {
   });
 }
 
+// eslint-disable-next-line jest/require-hook
 fastifyInstance.register(server, opts);
+// eslint-disable-next-line jest/require-hook
 fastifyInstance.register(fastifyBlipp);
 
 const start = () => fastifyInstance.listen(opts.app.port, '0.0.0.0', (err) => {
@@ -128,4 +131,5 @@ const start = () => fastifyInstance.listen(opts.app.port, '0.0.0.0', (err) => {
   fastifyInstance.log.info(`Redis cache enabled: ${!!opts.redis.enable}`);
 });
 
+// eslint-disable-next-line jest/require-hook
 start();
