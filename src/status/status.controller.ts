14ef9ae9d279b0442cb1ecc1882b6ff569c0bfe2
Łukasz Wójcik <lwojcik@lukem.net>
2022-06-21 21:15:50 +0200

    Add debug logs

--- src/status/status.controller.ts
+++ src/status/status.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 import { StatusResponse } from '../types';
 import { StatusService } from './status.service';
 
 @Controller('status')
 export class StatusController {
-  constructor(private readonly statusService: StatusService) {}
+  constructor(
+    private readonly statusService: StatusService,
+    @InjectPinoLogger(StatusController.name) private readonly logger: PinoLogger
+  ) {}
 
   @Get()
   getStatus(): StatusResponse {
+    this.logger.debug('StatusController.getStatus()');
     return this.statusService.getStatus();
   }
 }
--- src/status/status.service.ts
+++ src/status/status.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 import { formatTime } from '../utils/formatTime';
 import { StatusResponse } from '../types';
 
 @Injectable()
 export class StatusService {
+  constructor(
+    @InjectPinoLogger(StatusService.name) private readonly logger: PinoLogger
+  ) {}
+
   getStatus(): StatusResponse {
+    this.logger.debug('StatusService.getStatus()');
     return {
       status: 'ok',
       uptime: formatTime(process.uptime()),
