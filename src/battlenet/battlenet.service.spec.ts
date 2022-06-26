5da98a29b67784caaf8f039b88061bf77b7c03ef
Łukasz Wójcik <lwojcik@lukem.net>
2022-06-26 20:00:59 +0200

    More unit test coverage

--- src/battlenet/battlenet.service.spec.ts
+++ src/battlenet/battlenet.service.spec.ts
@@ -1,21 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, registerAs } from '@nestjs/config';
+import { BlizzAPI } from 'blizzapi';
 import { LoggerService } from '../logger/logger.service';
-import { battleNetConfig } from '../config';
 import { BattleNetService } from './battlenet.service';
+
+jest.mock('blizzapi');
 
 describe('BattleNetService', () => {
   let service: BattleNetService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule.forFeature(battleNetConfig)],
+      imports: [
+        ConfigModule.forFeature(
+          registerAs('battlenet', () => ({
+            region: 'sample-battlenet-region',
+            clientId: 'sample-client-id',
+            clientSecret: 'sample-client-secret',
+          }))
+        ),
+      ],
       providers: [
         {
           provide: LoggerService,
           useValue: {
             debug: jest.fn(),
             log: jest.fn(),
+            error: jest.fn(),
             setLoggedClass: jest.fn(),
             setLoggedMethod: jest.fn(),
           },
@@ -31,7 +42,28 @@ describe('BattleNetService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should implement getAccessToken()', () => {
-    expect(service.getAccessToken).toBeDefined();
+  it('should get access token', async () => {
+    BlizzAPI.prototype.getAccessToken = () =>
+      Promise.resolve('sample_access_token_from_blizzapi_mock');
+
+    expect.assertions(1);
+    const accessToken = await service.getAccessToken();
+    expect(accessToken).toMatchSnapshot();
+  });
+
+  it('should return error object in case of Battle.net API error', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (BlizzAPI as any).prototype.getAccessToken = () =>
+      // eslint-disable-next-line prefer-promise-reject-errors
+      Promise.reject({
+        message: 'Sample error message from BlizzAPI mock',
+        response: {
+          status: 'Sample error status from BlizzAPI mock',
+        },
+      });
+
+    expect.assertions(1);
+    const accessToken = await service.getAccessToken();
+    expect(accessToken).toMatchSnapshot();
   });
 });
