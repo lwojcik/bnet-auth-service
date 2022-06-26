0094150d7ce54d9e5edc988335c9b6e0b8515623
Łukasz Wójcik <lwojcik@lukem.net>
2022-06-26 23:05:54 +0200

    E2E test stubs

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
+// import request from 'supertest';
 import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('AppModule (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,6 +15,90 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () =>
-    request(app.getHttpServer()).get('/').expect(200).expect('Hello World!'));
+  // it('/ (GET)', () =>
+  //   request(app.getHttpServer()).get('/').expect(200).expect('Hello World!'));
+
+  describe('Authorization enabled', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
+
+  describe('Authorization disabled', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
+
+  describe('Redis cache enabled', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
+
+  describe('Redis cache disabled', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
+
+  describe('HTTPS enabled', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
+
+  describe('HTTPS disabled', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
+
+  describe('Rate limiting enabled', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
+
+  describe('Rate limiting disabled', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
+
+  describe('CORS enabled', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
+
+  describe('CORS disabled', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
+
+  describe('Battle.net credentials configured', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
+
+  describe('Battle.net credentials not configured', () => {
+    it.todo('/ (GET)');
+    it.todo('/status (GET)');
+    it.todo('/accesstoken (GET)');
+    it.todo('/accesstoken?refresh=true (GET)');
+  });
 });
