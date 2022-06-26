eddc6a68ba6710a1256c643e6b69ae6d8608a880
Łukasz Wójcik <lwojcik@lukem.net>
2022-06-27 01:00:06 +0200

    Env vars

--- src/app.module.spec.ts
+++ src/app.module.spec.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 describe('AppModule', () => {
   const { env } = process;
+  process.env.BAS_BATTLENET_REGION = 'us';
+  process.env.BAS_BATTLENET_CLIENT_ID = 'sample-client-id';
+  process.env.BAS_BATTLENET_CLIENT_SECRET = 'sample-client-secret';
 
   beforeEach(() => {
     jest.resetModules();
@@ -11,10 +14,6 @@ describe('AppModule', () => {
   });
 
   it('should compile the module', async () => {
-    process.env.BAS_BATTLENET_REGION = 'us';
-    process.env.BAS_BATTLENET_CLIENT_ID = 'sample-client-id';
-    process.env.BAS_BATTLENET_CLIENT_SECRET = 'sample-client-secret';
-
     const module = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
