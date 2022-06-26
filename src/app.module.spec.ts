b6525380593af1a885cff3b887765b1e0e4efe72
Łukasz Wójcik <lwojcik@lukem.net>
2022-06-27 00:55:41 +0200

    Fill environment variables in app.module.spec.ts

--- src/app.module.spec.ts
+++ src/app.module.spec.ts
@@ -3,11 +3,26 @@ import { AppController } from './app.controller';
 import { AppModule } from './app.module';
 
 describe('AppModule', () => {
+  const { env } = process;
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...env };
+  });
+
   it('should compile the module', async () => {
+    process.env.BAS_BATTLENET_REGION = 'us';
+    process.env.BAS_BATTLENET_CLIENT_ID = 'sample-client-id';
+    process.env.BAS_BATTLENET_CLIENT_SECRET = 'sample-client-secret';
+
     const module = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     expect(module.get(AppController)).toBeInstanceOf(AppController);
+  });
+
+  afterEach(() => {
+    process.env = env;
   });
 });
