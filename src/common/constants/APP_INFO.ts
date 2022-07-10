015c7a9d2a936f954af29a80d6ebf1d3001cd34b
Łukasz Wójcik <lwojcik@lukem.net>
2022-07-10 13:25:01 +0200

    Remove constants

--- src/common/constants/APP_INFO.ts
+++
@@ -1,6 +0,0 @@
-export const APP_INFO = {
-  name: 'bnet-auth-service',
-  description:
-    'REST microservice for retrieving and caching OAuth access tokens from Blizzard Battle.net API',
-  appPrefix: 'BAS',
-};
