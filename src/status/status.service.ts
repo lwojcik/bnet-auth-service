d4eb4b1a664686ab32d2f518f1e2cc5a3cbe7b14
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2022-07-10 14:25:35 +0200

    Remove utils (#635)
    * Remove utils
    
    * Update status endpoint so that it doesn't rely on locale

--- src/status/status.service.ts
+++ src/status/status.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { formatTime } from '../utils';
+import { Duration } from 'luxon';
 import { ApiStatus } from '../common/types';
 import { StatusResponse } from './dto/status-response.dto';
 
 @Injectable()
 export class StatusService {
   getStatus(): StatusResponse {
+    const uptimeSeconds = Math.floor(process.uptime());
+    const uptimeMinutes = Math.floor(uptimeSeconds / 60);
+    const uptimeHours = Math.floor(uptimeMinutes / 60);
+    const uptimeDays = Math.floor(uptimeHours / 24);
+
     return {
       status: ApiStatus.ok,
-      uptime: formatTime(process.uptime()),
+      uptime: Duration.fromObject({
+        days: uptimeDays,
+        hours: uptimeHours,
+        minutes: uptimeMinutes,
+      }).toHuman({
+        unitDisplay: 'narrow',
+      }),
       timestamp: new Date().toISOString(),
     };
   }
