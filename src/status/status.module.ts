537ad4c26774a022549f903176ba793fcc792924
Łukasz Wójcik <lwojcik@lukem.net>
2022-07-12 02:57:19 +0200

    Remove unused imports

--- src/status/status.module.ts
+++ src/status/status.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { authConfig } from '../config';
 import { StatusController } from './status.controller';
 import { StatusService } from './status.service';
 
 @Module({
-  imports: [ConfigModule.forFeature(authConfig)],
   controllers: [StatusController],
   providers: [StatusService],
 })
